151921641e17512b0a8026aef76bd5b9b38aa098
Alan Ashworth <awashworth927@gmail.com>
2020-05-11 07:20:03 -0400

    Initialize sever with React client.

---
+++ server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+// const routes = require("./routes");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Test for running server
+app.get("/api/config", (req, res) => {
+    res.json({
+      success: true,
+      currentPort: PORT,
+    });
+  });
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Define API routes here
+// app.use(routes)
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/playlistoflegends", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API Server listening on http://localhost:${PORT}`);
+});
